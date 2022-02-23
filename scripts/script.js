15c50441ab0148b3c53c3f7eda5eaf6cae745095
Andreas Schaefar <skachko.andrii@gmail.com>
2022-02-23 15:52:42 +0200

    Second iteration with class Counter

--- scripts/script.js
+++ scripts/script.js
@@ -7,23 +7,46 @@ const second = 1000;
 const minute = second * 60;
 const hour = minute * 60;
 const day = hour * 24;
+const newYearDate = "1/01/2023";
 
-let newYearDate = " 1/01/2023";
-let newYear = new Date(newYearDate);
+class Counter {
 
+    constructor(newYearDate, days, hours, minutes, seconds) {
+        this.newYearDate = new Date(newYearDate);
+        this.days = days;
+        this.hours = hours;
+        this.minutes = minutes;
+        this.seconds = seconds;
+    }
 
-function setTime() {
-    let currentDate = new Date();
-    let timeDiff = newYear - currentDate;
-    let dayDiff = timeDiff / day;
-    let hourDiff = dayDiff % 1 * day / hour;
-    let minDiff = hourDiff % 1 * hour / minute;
-    let secDiff = minDiff % 1 * minute / second;
-    
-    days.textContent = Math.trunc(dayDiff);
-    hours.textContent = Math.trunc(hourDiff);
-    minutes.textContent = Math.trunc(minDiff);
-    seconds.textContent = Math.trunc(secDiff);
+    static getTime(unit, difference) {
+        switch (unit) {
+            case "day":
+                return difference / day;
+            case "hour":
+                return (difference / day) % 1 * day / hour;
+            case "minute":
+                return ((difference / day) % 1 * day / hour) % 1 * hour / minute;
+            case "second":
+                return (((difference / day) % 1 * day / hour) % 1 * hour / minute) % 1 * minute / second;
+            default:
+                break;
+        }
+    }
+
+    static truncate(value) {
+        return Math.trunc(value);
+    }
+
+    setTime() {
+        let currentDate = new Date();
+        let timeDiff = this.newYearDate - currentDate;
+        this.days.textContent = Counter.truncate(Counter.getTime("day", timeDiff));
+        this.hours.textContent = Counter.truncate(Counter.getTime("hour", timeDiff));
+        this.minutes.textContent = Counter.truncate(Counter.getTime("minute", timeDiff));
+        this.seconds.textContent = Counter.truncate(Counter.getTime("second", timeDiff));
+    }
 }
 
-setInterval(setTime, 1000);
+const counter = new Counter(newYearDate, days, hours, minutes, seconds);
+setInterval(() => counter.setTime(), 1000);
